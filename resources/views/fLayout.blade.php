0df527f4374dd212c4000bebc58a12be95c45c7a
muhammad irfan thoriq <irfanthq04@gmail.com>
2023-11-01 08:59:44 +0700

    Pesan commit

--- resources/views/fLayout.blade.php
+++ resources/views/fLayout.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Corinthia:700|Poppins:300&display=swap|Poppins:600&display=swap">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Poppins:wght@300;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Poppins:wght@300;700&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <!-- boostrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('../assets/dataTables/datatables.min.css') }}">
         <link href="https://cdn.datatables.net/v/dt/dt-1.13.4/datatables.min.css" rel="stylesheet" />
 </head>
@@ -63,10 +63,8 @@
     </script>
 
     <!-- boostrap js -->
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="{{ asset('../assets/dataTables/datatables.min.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
@@ -78,6 +76,8 @@
   <script>
     AOS.init();
   </script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
 </body>
 
 </html>
--- resources/views/frontend/landingpage.blade.php
+++ resources/views/frontend/landingpage.blade.php
@@ -7,6 +7,8 @@
         <div class="title">
             <h1 class="corinthia-text">Pemilihan</h1>
             <h2 class="poppins-text">Duta GenRe Sumatera Selatan 2023</h2>
+            <a href="#" class="votenow-button">Vote</a>
+            <a href="#" class="buy-button">Beli Voucher</a>
         </div>
     </div>
     <div class="tentang">
@@ -28,8 +30,140 @@
                     <li>Menjadi tempat konseling teman sebaya atau remaja agar dapat memberikan solusi bagi permasalahan remaja yang ada dengan memasukkan nilai-nilai GenRe di dalamnya.</li>
                     <li>Menjadi role model yang memiliki masa depan terencana bagi remaja masa kini agar memiliki kualitas hidup yang tinggi. Menjadi remaja dan mahasiswa yang berperilaku sehat dan berakhlak mulia.</li>
                 </ul>
+                <a href="#" class="more-button">Selengkapnya</a>
             </div>
+        </div>
+        <div class="kegiatan">
+            <img src="images/kegiatan.png" alt="21" class="kegiatan" />
+        </div>
+        <div class="dukungan">
+            <div class="kotak">
+                <p>Berikan Dukungan Kepada Peserta Pemilihan Duta GenRe Sumatera Selatan 2023</p>
+                <div class="gabung">
+                    <a href="#" class="votenoww-button">Vote</a>
+                    <a href="#" class="buyy-button">Beli Voucher</a>
+                </div>
+
+            </div>
+        </div>
+        <div class="sejarah">
+            <h1 class="corinthia-text">Sejarah</h1>
+            <h2 class="poppins-text">Duta GenRe Sumatera Selatan</h2>
+            <br>
+            <p>Program Genre (Generasi berencana) diinisiasi pada tahun 2007 oleh Bapak Dr. Sudibyo Alimoeso yang saat itu menjabat sebagai Deputi KSPK BKKBN. Program ini dilatarbelakangi oleh banyaknya kenakalan remaja yang terjadi. Seperti seksualitas, penyalahgunaan narkoba, seks bebas dan nikah muda. <br><br>
+
+                Fenomena kenakalan remaja yang terjadi diiringi dengan jumlah remaja yang hampir 30% dari penduduk Indonesia. Hal ini tentunya menjadi fokus Badan Kependudukan dan Keluarga Berencana Nasional karena terkait dengan kependudukan. Terlebih, remaja yang merupakan generasi penerus bangsa, perlu dipersiapkan menjadi manusia yang sehat secara jasmani, rohani, mental dan spiritual. <br><br>
+
+                Oleh karena itu diperlukan suatu program yang dapat memberikan informasi yang berkaiatan dengan penyiapan diri remaja menyongsong kehidupan berkeluarga yang lebih baik, menyiapakan pribadi yang matang dalam membangun keluarga yang harmonis, dan memantapkan perencanaan dalam menata kehidupan untuk keharmonisan keluarga. Sebagai Implementasi Undang-Undang Nomor 52 tahun 2009, tentang perkembangan kependudukan dan pembangunan keluarga, pasal 48 ayat 1 (b) yang mengatakan bahwa “peningkatan kualitas remaja dengan dengan pemberian akases informasi, pendidikan, konseling, dan pelayanan tentang kehidupan berkeluarga,” maka BKKBN sebagai salah satu istitusi pemerintah harus mewujudkan tercapainya peningkatan kualitas remaja melalui program Generasi Berencana. <br><br>
+
+                Melalui program GenRe, BKKBN membuat Pusat Informasi Konseling Remaja dan Mahasiswa (PIK R/M) dan bekerja sama dengan para mahasiswa dengan mengadakan Pemilihan Duta Mahasiswa GenRe. <br><br>
+
+                BKKBN Perwakilan Sumatera Selatan selaku pendukung jalannya program BKKBN pusat juga melaksanakan semua program kerja termasuk mengadakan Pemilihan Duta Mahasiswa GenRe di wilayah provinsi Sumatera Selatan.</p>
+            <a href="#" class="more-button">Selengkapnya</a>
+        </div>
+        <div class="timelinee">
+            <h3 class="corinthia-text">Dari Masa ke Masa</h3>
+            <h4 class="poppins-text">Duta GenRe Sumatera Selatan</h4>
+            <br><br>
+            <section class="timeline">
+                <ul>
+                    <li>
+                        <div>
+                            <time>1934</time> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1937</time> Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1940</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1943</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1946</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1956</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1957</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1967</time> Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus, euismod quis ante.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1977</time> Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quam dolor convallis enim, non dignissim ligula ipsum a turpis.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>1985</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>2000</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                        </div>
+                    </li>
+                </ul>
+            </section>
         </div>
     </div>
 </div>
+<script>
+    (function () {
+  "use strict";
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
+</script>
 @endsection
